4d5c28eae080029ad4a7496bb8c50739a13b3a03
Srujana Gudla <srujanagudla79@gmail.com>
2026-07-21 21:40:54 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome message
+window.onload = function () {
+    alert("Welcome to My Portfolio Website!");
+};
+
+// Contact form message
+function submitForm() {
+    alert("Thank you! Your message has been submitted.");
+}
